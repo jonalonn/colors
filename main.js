4b32e3a2d85fb8a4e349831d412441893c1a5a8f
Jonathan <jonathan.lonnqvist@gmail.com>
2013-11-06 23:33:43 +0100

    Random colours

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-$( document ).ready(function() {
-var c=document.getElementById("colorGrid");
-	var ctx=c.getContext("2d");
-	var leftMargin = 0;
-	var topMargin = 0;
-	var colors = ["red", "blue", "green", "yellow", "orange", "brown", "purple", "pink", "grey", "black"]
-	var cellHeight = 2;
-	var cellWidth = 2;
-	var windowHeight = window.innerHeight;
-	var windowWidth = window.innerWidth;
-	var rowEnd = windowWidth;
-	colorGrid.width = windowWidth;
-	colorGrid.height = windowHeight;
+var c=document.getElementById("colorCanvas");
+var ctx=c.getContext("2d");
+var leftMargin = 0;
+var topMargin = 0;
+var colors = []
+var cellHeight = 2;
+var cellWidth = 2;
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var rowEnd = windowWidth;
+colorCanvas.width = windowWidth;
+colorCanvas.height = windowHeight;
+
+function randomColor() {
+	for (i=0;i<10;i++) {
+		var rC = Math.floor(Math.random()*16777215).toString(16);
+		colors[i] = rC
+		$('.num' + i).val(rC)
+	}
+	colors=[]
+}
+
+randomColor();
 
 function drawNumber(fileName) {
 	var request = new XMLHttpRequest();
@@ -18,6 +28,7 @@ function drawNumber(fileName) {
 	request.send(null);
 	var response = request.responseText;
 	var digits = response.toString().split('');
+	console.log("functioncolor" + colors)
 	for (i=0;i<digits.length;i++) {
         // Returns what row you are on
         // Example, if you are on #350, then 350 / 128 will be 2.72. We "floor" it to remove the decimal, leaving it at just 2. It will be a value of 2 from #256 all the way to 383
@@ -27,25 +38,24 @@ function drawNumber(fileName) {
         var topMargin = row * cellHeight;
         var leftMargin = col * cellWidth;
         ctx.fillStyle=colors[digits[i]];
-        ctx.fillRect(leftMargin, topMargin, cellWidth, cellWidth);
+        ctx.fillRect(leftMargin, topMargin, cellWidth, cellHeight);
     }
+    $('#reloadButton').show();
 }
-
-
-  $('.numButton').click(function() {
-  	var num1 = $('.num1').val()
-  	var num2 = $('.num2').val()
-  	var num3 = $('.num3').val()
-  	var num4 = $('.num4').val()
-  	var num5 = $('.num5').val()
-  	var num6 = $('.num6').val()
-  	var num7 = $('.num7').val()
-  	var num8 = $('.num8').val()
-  	var num9 = $('.num9').val()
-  	var num0 = $('.num0').val()
-  	var fileName = $(this).val();
-  	colors = [num1, num2, num3, num4, num5, num6, num7, num8, num9, num0]
-  	$('#inputDiv').hide();
-  	drawNumber(fileName);
-  })
-});
+$('#reloadButton').hide();
+$('.restart').click(function() {
+	location.reload();
+})
+$('.numButton').click(function() {
+	for (i=0;i<10;i++) {
+		colors.push($('.num' + i).val())
+	}
+	console.log("clickcolor" + colors)
+	var pW = $('.pixWidth').val()
+	var pH = $('.pixHeight').val()
+	var fileName = $(this).val();
+	cellHeight = pH;
+	cellWidth = pW;
+	$('#inputDiv').hide();
+	drawNumber(fileName);
+})
